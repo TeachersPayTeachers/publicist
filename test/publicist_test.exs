c3845c06889945fea6303580163dc860a3ab8014
Victor Deryagin <vderyagin@gmail.com>
2018-01-26 22:29:53 +0200

    Support publicising private macros (#2)
    * Add `defmacrop` definition
    
    * Replace Kernel's defmacrop with ours
    
    * Update moduledoc
    
    * Test

--- test/publicist_test.exs
+++ test/publicist_test.exs
@@ -3,13 +3,20 @@ defmodule PublicistTest.Foo do
   defp bar() do
     0
   end
+
+  defmacrop bar_macro, do: :bar
 end
 
 defmodule PublicistTest do
   use ExUnit.Case
   doctest Publicist
 
-  test "PublicistTest.Foo.bar is accessible" do
+  test "PublicistTest.Foo.bar function is accessible" do
     assert PublicistTest.Foo.bar() === 0
+  end
+
+  test "PublicistTest.Foo.bar_macro macro is accessible" do
+    require PublicistTest.Foo
+    assert PublicistTest.Foo.bar_macro() === :bar
   end
 end
